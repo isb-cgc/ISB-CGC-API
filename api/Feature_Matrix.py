9b3194823b15baae1a91d64e0a2b8948180df6f8
phyllers <plee@systemsbiology.org>
2015-10-30 11:48:37 -0700

    Added helper functions for data access.
    Still need to refactor our django models from metadata.py and Cohorts.py.

--- api/Feature_Matrix.py
+++ api/Feature_Matrix.py
@@ -6,7 +6,6 @@ from google.appengine.ext import ndb
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
-from django.contrib.auth.models import User
 
 from api_helpers import *
 
--- cgc_api.py
+++ cgc_api.py
@@ -5,7 +5,7 @@ from api.maf import MAFEndpointsAPI
 from api.feature_access_v2 import FeatureAccessEndpoints
 from api.data_access import FeatureDataEndpoints
 from api.big_query import BQ_Endpoints
-from api.Cohorts import Cohort_Endpoints
+# from api.Cohorts import Cohort_Endpoints
 from api.metadata import Meta_Endpoints
 from api.pairwise_api import Pairwise_Endpoints
 package = 'isb-cgc-api'
@@ -13,7 +13,7 @@ package = 'isb-cgc-api'
 
 APPLICATION = endpoints.api_server([
     MAFEndpointsAPI,
-    Cohort_Endpoints,
+    # Cohort_Endpoints,
     BQ_Endpoints,
     FeatureAccessEndpoints,
     Meta_Endpoints,
