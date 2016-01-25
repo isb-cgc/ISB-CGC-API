cd431dc35e34fd325dca3ccb684a71fc7d2de883
phyllers <phyllers@gmail.com>
2016-01-24 19:23:18 -0800

    adding appengine to python path

--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -39,8 +39,9 @@ echo "Installing Google App Engine..."
 wget -q https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.31.zip -O ${HOME}/google_appengine.zip
 unzip -nq ${HOME}/google_appengine.zip -d $HOME
 export PATH=$PATH:${HOME}/google_appengine/
-mkdir ${HOMEROOT}/lib/endpoints/ 2> /dev/null
-cp ${HOME}/google_appengine/lib/endpoints-1.0/endpoints/* ${HOMEROOT}/lib/endpoints/
+export PYTHONPATH=$PYTHONPATH:${HOME}/google_appengine/
+#mkdir ${HOMEROOT}/lib/endpoints/ 2> /dev/null
+#cp ${HOME}/google_appengine/lib/endpoints-1.0/endpoints/* ${HOMEROOT}/lib/endpoints/
 echo "Google App Engine Installed"
 
 # Install Google Cloud SDK
