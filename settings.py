7a02f5e5898e7441f76a743217e0d0bda644ae59
phyllers <phyllers@gmail.com>
2016-01-28 17:21:04 -0800

    db password mandatory

--- settings.py
+++ settings.py
@@ -68,12 +68,10 @@ DATABASES = {
         'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DATABASE_PORT', 3306),
         'NAME': os.environ.get('DATABASE_NAME', 'dev'),
-        'USER': os.environ.get('DATABASE_USER')
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD')
     }
 }
-
-if os.environ.has_key('DATABASE_PASSWORD'):
-    DATABASES['default']['PASSWORD'] = os.environ.get('DATABASE_PASSWORD')
 
 if os.environ.has_key('DB_SSL_CERT'):
     DATABASES['default']['OPTIONS'] = {
