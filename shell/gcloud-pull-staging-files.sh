8fba455a75fddcaaa7b3af610b1d37458fbb18f9
phyllers <phyllers@gmail.com>
2016-04-08 12:15:54 -0700

    Changed deployment scripts

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -5,4 +5,4 @@
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/ISB-CGC-uat-client_secrets.json" ./client_secrets.json
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/ISB-CGC-uat-privatekey2.json" ./privatekey.json
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/ISB-CGC-uat-privatekey2.pem" ./privatekey.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${DEV_OAUTH_TOKEN}" ~/.appcfg_oauth2_tokens
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/.appcfg_oauth2_tokens" ~/.appcfg_oauth2_tokens
