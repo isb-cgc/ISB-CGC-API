f3f96a1be899884f7495a87ae289d1f10c98c468
phyllers <phyllers@gmail.com>
2016-04-13 11:58:47 -0700

    debugging

--- api/metadata.py
+++ api/metadata.py
@@ -1744,6 +1744,7 @@ class Meta_Endpoints_API_v2(remote.Service):
         study_ids = ()
         cohort_id = None
         user = get_current_user(request)
+        print request
         if request.__getattribute__('filters') is not None:
             try:
                 tmp = json.loads(request.filters)
