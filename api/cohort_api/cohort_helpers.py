e329aec2efb24f0c215b748822b6366277325983
medullaskyline <medullaskyline@gmail.com>
2016-04-28 18:29:55 -0700

    starting refactor of preview

---
+++ api/cohort_api/cohort_helpers.py
@@ -0,0 +1,28 @@
+
+import endpoints
+
+from django.conf import settings
+from functools import wraps
+from django.db import close_connection
+
+INSTALLED_APP_CLIENT_ID = settings.INSTALLED_APP_CLIENT_ID
+CONTROLLED_ACL_GOOGLE_GROUP = settings.ACL_GOOGLE_GROUP
+
+
+Cohort_Endpoints = endpoints.api(name='cohort_api', version='v1',
+                                 description="Get information about cohorts, patients, and samples. Create and delete cohorts.",
+                                 allowed_client_ids=[INSTALLED_APP_CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID])
+
+
+def cloud_endpoints_close_connection(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            endpoint = f(*args, **kwargs)
+            close_connection()
+            return endpoint
+        except:
+            close_connection()
+            raise
+
+    return decorated
