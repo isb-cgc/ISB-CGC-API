a808964b0b6312609884aa062f08ccc653e703c6
medullaskyline <medullaskyline@gmail.com>
2016-05-18 17:20:28 -0700

    CohortsList duplicate name fixed

--- api/cohort_api/cohorts_list.py
+++ api/cohort_api/cohorts_list.py
@@ -4,6 +4,7 @@ import logging
 import MySQLdb
 import django
 
+from pprint import pprint
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.contrib.auth.models import User as Django_User
@@ -15,13 +16,15 @@ from api.api_helpers import sql_connection, get_user_email_from_token
 
 logger = logging.getLogger(__name__)
 
+BASE_URL = settings.BASE_URL
+
 
 class FilterDetails(messages.Message):
     name = messages.StringField(1)
     value = messages.StringField(2)
 
 
-class Cohort(messages.Message):
+class CohortDetails(messages.Message):
     id = messages.StringField(1)
     name = messages.StringField(2)
     last_date_saved = messages.StringField(3)
@@ -36,21 +39,20 @@ class Cohort(messages.Message):
     num_samples = messages.StringField(12)
 
 
-class CohortsList(messages.Message):
-    items = messages.MessageField(Cohort, 1, repeated=True)
+class CohortDetailsList(messages.Message):
+    items = messages.MessageField(CohortDetails, 1, repeated=True)
     count = messages.IntegerField(2)
-
-BASE_URL = settings.BASE_URL
 
 
 class CohortsListQueryBuilder(object):
+
     def build_cohort_query(self, query_dict):
-        '''
+        """
         Builds the query that will select cohort id, name, last_date_saved,
         perms, comments, source type, and source notes
         :param query_dict: should contain {'cohorts_cohort_perms.user_id': user_id, 'cohorts_cohort.active': unicode('1')}
         :return: query_str, query_tuple
-        '''
+        """
         query_str = 'SELECT cohorts_cohort.id, ' \
                     'cohorts_cohort.name, ' \
                     'cohorts_cohort.last_date_saved, ' \
@@ -87,61 +89,60 @@ class CohortsListQueryBuilder(object):
         return query_str, query_tuple
 
     def build_filter_query(self, filter_query_dict):
-        '''
+        """
         Builds the query that selects the filter name and value for a particular cohort
         :param filter_query_dict: should be {'cohorts_filters.resulting_cohort_id:': id}
         :return: filter_query_str, filter_query_tuple
-        '''
+        """
         filter_query_str = 'SELECT name, value ' \
                            'FROM cohorts_filters '
 
-        filter_query_str += ' WHERE ' + '=%s and '.join(key for key in filter_query_dict.keys()) + '=%s '
+        filter_query_str += ' WHERE ' + '=%s AND '.join(key for key in filter_query_dict.keys()) + '=%s '
         filter_query_tuple = tuple(value for value in filter_query_dict.values())
 
         return filter_query_str, filter_query_tuple
 
     def build_parent_query(self, parent_query_dict):
-        '''
+        """
         Builds the query that selects parent_ids for a particular cohort
         :param parent_query_dict: should be {'cohort_id': str(row['id'])}
         :return: parent_query_str, parent_query_tuple
-        '''
-        parent_query_str ='SELECT parent_id ' \
-                          'FROM cohorts_source ' \
-                          'WHERE cohort_id=%s'
-        parent_query_str += ' WHERE ' + '=%s and '.join(key for key in parent_query_dict.keys()) + '=%s '
+        """
+        parent_query_str = 'SELECT parent_id ' \
+                           'FROM cohorts_source '
+        parent_query_str += ' WHERE ' + '=%s AND '.join(key for key in parent_query_dict.keys()) + '=%s '
         parent_query_tuple = tuple(value for value in parent_query_dict.values())
 
         return parent_query_str, parent_query_tuple
 
-# probably don't use this
-class CohortsListMessageListBuilder(object):
-    def build_message_list(self, message_class, cursor_row_results):
-        # cursor_row_results = {'name': 'aname', 'value': 'avalue'}
-        # [field.name for field in message_class.all_fields()] = ['name', 'value']
-        return_data = []
-        for row in cursor_row_results:
-            message_class_instance = message_class()
-            for field in message_class.all_fields():
-                setattr(message_class_instance, field.name, row[field.name])
-            return_data.append(message_class_instance)
 
-        return return_data
-
+# # probably don't use this
+# class CohortsListMessageListBuilder(object):
+#     def build_message_list(self, message_class, cursor_row_results):
+#         # cursor_row_results = {'name': 'aname', 'value': 'avalue'}
+#         # [field.name for field in message_class.all_fields()] = ['name', 'value']
+#         return_data = []
+#         for row in cursor_row_results:
+#             message_class_instance = message_class()
+#             for field in message_class.all_fields():
+#                 setattr(message_class_instance, field.name, row[field.name])
+#             return_data.append(message_class_instance)
+#
+#         return return_data
 
 
 @Cohort_Endpoints.api_class(resource_name='cohort_endpoints')
 class CohortsList(remote.Service):
     GET_RESOURCE = endpoints.ResourceContainer(token=messages.StringField(1), cohort_id=messages.IntegerField(2))
 
-    @endpoints.method(GET_RESOURCE, CohortsList,
+    @endpoints.method(GET_RESOURCE, CohortDetailsList,
                       path='cohorts_list2', http_method='GET', name='cohorts.list2')
     def cohorts_list2(self, request):
-        '''
+        """
         Returns information about cohorts a user has either READER or OWNER permission on.
         Authentication is required. Optionally takes a cohort id as a parameter to
         only list information about one cohort.
-        '''
+        """
         user_email = None
         cursor = None
         filter_cursor = None
@@ -180,7 +181,7 @@ class CohortsList(remote.Service):
         query_str, query_tuple = CohortsListQueryBuilder().build_cohort_query(query_dict)
 
         filter_query_str = ''
-        parent_id_query_str = ''
+        parent_query_str = ''
         row = None
 
         try:
@@ -190,7 +191,7 @@ class CohortsList(remote.Service):
             data = []
 
             for row in cursor.fetchall():
-                filter_query_dict = {'cohorts_filters.resulting_cohort_id:': str(row['id'])}
+                filter_query_dict = {'cohorts_filters.resulting_cohort_id': str(row['id'])}
                 filter_query_str, filter_query_tuple = CohortsListQueryBuilder().build_filter_query(filter_query_dict)
 
                 filter_cursor = db.cursor(MySQLdb.cursors.DictCursor)
@@ -214,7 +215,7 @@ class CohortsList(remote.Service):
                 parent_cursor.execute(parent_query_str, parent_query_tuple)
                 parent_id_data = [int(p_row['parent_id']) for p_row in parent_cursor.fetchall() if row.get('parent_id')]
 
-                data.append(Cohort(
+                data.append(CohortDetails(
                     id=str(row['id']),
                     name=str(row['name']),
                     last_date_saved=str(row['last_date_saved']),
@@ -231,16 +232,19 @@ class CohortsList(remote.Service):
                 optional_message = " matching cohort id " + str(cohort_id) if cohort_id is not None else ""
                 raise endpoints.NotFoundException("{} has no active cohorts{}."
                                                   .format(user_email, optional_message))
-            return CohortsList(items=data, count=len(data))
+            return CohortDetailsList(items=data, count=len(data))
+
         except (IndexError, TypeError) as e:
             raise endpoints.NotFoundException(
                 "User {}'s cohorts not found. {}: {}".format(user_email, type(e), e))
+
         except MySQLdb.ProgrammingError as e:
-            msg = '{}:\n\tcohort query: {} {}\n\tfilter query: {} {}\n\tparent id query: {} {}' \
-                .format(e, query_str, query_tuple, filter_query_str, str(row['id']), parent_id_query_str,
+            msg = '{}:\n\tcohort query: {} {}\n\tfilter query: {} {}\n\tparent query: {} {}' \
+                .format(e, query_str, query_tuple, filter_query_str, str(row['id']), parent_query_str,
                         str(row['id']))
             logger.warn(msg)
             raise endpoints.BadRequestException("Error retrieving cohorts or filters. {}".format(msg))
+
         finally:
             if cursor: cursor.close()
             if filter_cursor: filter_cursor.close()
