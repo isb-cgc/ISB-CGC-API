97e66b97ce1e9edb4e63ec74e777d674777ec019
phyllers <phyllers@gmail.com>
2016-06-01 15:02:11 -0700

    Trying to setup django

--- settings.py
+++ settings.py
@@ -15,6 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
+import django
+django.setup()
 
 # Django settings for GAE_Django17 project.
 import os
