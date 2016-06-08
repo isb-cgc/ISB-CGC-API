abdfd4441cc40f5c698e12f8f6aaf1c405b171e2
kleinone <kleinone@systemsbiology.org>
2016-06-08 14:16:55 -0700

    Add shared code module path to Python module path

--- appengine_config.py
+++ appengine_config.py
@@ -1,3 +1,16 @@
+import os
+import sys
+
 from google.appengine.ext import vendor
 # Add any libraries installed in the "lib" folder.
 vendor.add('lib')
+
+BASE_DIR                = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + os.sep
+
+SHARED_SOURCE_DIRECTORIES = [
+   os.path.abspath(os.environ.get('ISB_CGC_COMMON_MODULE_PATH'))
+]
+
+# Add the shared Django application subdirectory to the Python module search path
+for path in SHARED_SOURCE_DIRECTORIES:
+    sys.path.append(path)
