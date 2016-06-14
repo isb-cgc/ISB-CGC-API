ca448c4f4183769b562c049d18ea32756e85b211
medullaskyline <medullaskyline@gmail.com>
2016-06-13 23:19:03 -0700

    cleanup

--- cgc_api.py
+++ cgc_api.py
@@ -18,13 +18,15 @@ limitations under the License.
 
 import endpoints
 
-from api.feature_access import FeatureAccessEndpoints           # class FeatureAccessEndpoints(remote.Service) decorated with @FeatureAccessEndpointsAPI.api_class which is defined as FeatureAccessEndpointsAPI = endpoints_api(name='feature_type_api', ...)
+from api.feature_access import FeatureAccessEndpoints
+from api.single_feature_access import SingleFeatureDataAccess
+from api.data_access import FeatureDataEndpoints
+from api.metadata import Meta_Endpoints, Meta_Endpoints_v2
+from api.pairwise_api import Pairwise_Endpoints
+from api.seqpeek_view_api import SeqPeekViewDataAccessAPI
 
-from api.single_feature_access import SingleFeatureDataAccess   # class SingleFeatureDataAccess(remote.Service): decorated with @FeatureDataEndpointsAPI.api_class which is defined as FeatureAccessEndpointsAPI = endpoints_api(name='feature_type_api', ...)
-from api.data_access import FeatureDataEndpoints                # class FeatureDataEndpoints(remote.Service): decorated with @FeatureDataEndpointsAPI.api_class which is defined as FeatureAccessEndpointsAPI = endpoints_api(name='feature_type_api', ...)
+from api.Cohort import Cohort_Endpoints_API
 
-from api.Cohort import Cohort_Endpoints_API                     # class Cohort_Endpoints_API(remote.Service): decorated with @Cohort_Endpoints.api_class which is defined as Cohort_Endpoints = endpoints.api(name='cohort_api',..)
-# following decorated with @ISB_CGC_Endpoints.api_class(resource_name='cohorts', 'participants', 'samples', or 'users')
 from api.isb_cgc_api.cohorts_list import CohortsListAPI
 from api.isb_cgc_api.cohorts_preview import CohortsPreviewAPI
 from api.isb_cgc_api.cohorts_get import CohortsGetAPI
@@ -38,15 +40,21 @@ from api.isb_cgc_api.samples_datafilenamekeys import SamplesDatafilenamekeysAPI
 from api.isb_cgc_api.samples_googlegenomics import SamplesGoogleGenomicsAPI
 from api.isb_cgc_api.users_get import UserGetAPI
 
-from api.metadata import Meta_Endpoints, Meta_Endpoints_v2      # endpoints.api(name='meta_api', ...) @Meta_Endpoints.api_class
-from api.pairwise_api import Pairwise_Endpoints                 # endpoints.api(name='pairwise',...) @Pairwise_Endpoints.api_class
-from api.seqpeek_view_api import SeqPeekViewDataAccessAPI       # class SeqPeekViewDataAccessAPI(remote.Service): decprated with @SeqPeekDataEndpointsAPI.api_class which is defined as SeqPeekDataEndpointsAPI = endpoints_api(name='seqpeek_data_api', ...)
 
 package = 'isb-cgc-api'
 
 
 APPLICATION = endpoints.api_server([
+    FeatureAccessEndpoints,
+    Meta_Endpoints,
+    Meta_Endpoints_v2,
+    FeatureDataEndpoints,
+    SingleFeatureDataAccess,
+    Pairwise_Endpoints,
+    SeqPeekViewDataAccessAPI,
+
     Cohort_Endpoints_API,  # v1
+
     CohortsListAPI,
     CohortsPreviewAPI,
     CohortsGetAPI,
@@ -59,13 +67,5 @@ APPLICATION = endpoints.api_server([
     SamplesDatafilenamekeysAPI,
     SamplesGoogleGenomicsAPI,
     UserGetAPI,
-
-    FeatureAccessEndpoints,
-    Meta_Endpoints,
-    Meta_Endpoints_v2,
-    FeatureDataEndpoints,
-    SingleFeatureDataAccess,
-    Pairwise_Endpoints,
-    SeqPeekViewDataAccessAPI
 ])
 
