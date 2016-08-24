e74e51c6dbd0df08f2fd1afe6aad629c11b8fad4
medullaskyline <medullaskyline@gmail.com>
2016-08-23 18:44:12 -0700

    annotations for patients

--- api/isb_cgc_api/message_generator.py
+++ api/isb_cgc_api/message_generator.py
@@ -6,7 +6,7 @@ def get_sql_connection(args):
     try:
         connect_options = {
             'host': args.host,
-            'db': args.name,
+            'db': args.database,
             'user': args.user,
             'passwd': args.password,
             'ssl': {
@@ -28,7 +28,8 @@ FIELD_TYPES ={
     'int': 'IntegerField'
 }
 
-def write_file(rows):
+
+def write_metadata_file(rows, write_file=False):
 
     ranges_text = 'class MetadataRangesItem(messages.Message):\n    '
     i = 1
@@ -58,38 +59,75 @@ def write_file(rows):
         item_text += ')\n    ' if field_type is not 'IntegerField' else ', variant=messages.Variant.INT32)\n    '
         i += 1
 
-    with open('message_classes.py', 'w') as f:
-        f.write('from protorpc import messages\n\n\n')
-        f.write(ranges_text)
-        f.write(item_text)
+    if write_file is True:
+        with open('message_classes.py', 'w') as f:
+            f.write('from protorpc import messages\n\n\n')
+            f.write(ranges_text)
+            f.write(item_text)
+    else:
+        print ranges_text
+        print '\n\n'
+        print item_text
+
+
+def write_annotation_file(rows, write_file=False):
+    item_text = '\nclass MetadataAnnotationItem(messages.Message):\n    '
+    i = 1
+    for row in rows:
+        field_type = FIELD_TYPES.get(row['DATA_TYPE'])
+        item_text += '%-30s = messages.%s(%d' % (row['COLUMN_NAME'], field_type, i)
+        item_text += ')\n    ' if field_type is not 'IntegerField' else ', variant=messages.Variant.INT32)\n    '
+        i += 1
+
+    if write_file is True:
+        with open('message_classes.py', 'a') as f:
+            f.write('\n\n\n')
+            f.write(item_text)
+    else:
+        print item_text
 
 
 def main(args):
     db = get_sql_connection(args)
     cursor = db.cursor(MySQLdb.cursors.DictCursor)
-    query_str = '''
-    SELECT COLUMN_NAME, DATA_TYPE, COLUMN_TYPE
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE table_name = 'metadata_samples'
-    AND COLUMN_NAME != 'metadata_samples_id'
-    ORDER BY column_name
-    '''
+    query_str = 'SELECT COLUMN_NAME, DATA_TYPE, COLUMN_TYPE ' \
+                'FROM INFORMATION_SCHEMA.COLUMNS '
+    if args.table == 'metadata_samples':
+        query_str += 'WHERE table_name = "metadata_samples" ' \
+                'AND COLUMN_NAME != "metadata_samples_id" '
+
+    elif args.table == 'metadata_annotation':
+        query_str += 'WHERE table_name = "metadata_annotation" ' \
+                'AND COLUMN_NAME != "metadata_annotation_id" '
+
+    query_str += 'group by COLUMN_NAME, DATA_TYPE, COLUMN_TYPE ' \
+                 'ORDER BY column_name'
+
     cursor.execute(query_str)
     rows = cursor.fetchall()
     cursor.close()
     db.close()
 
-    write_file(rows)
+    write_file = not bool(args.dry_run)
+
+    if args.table == 'metadata_samples':
+        write_metadata_file(rows, write_file=write_file)
+    if args.table == 'metadata_annotation':
+        write_annotation_file(rows, write_file=write_file)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--host', help='database host')
-    parser.add_argument('--name', '-n', help='database name')
+    parser.add_argument('--database', '-d', help='database name')
+    parser.add_argument('--table', '-t', help='database table name')
     parser.add_argument('--user', '-u', help='database username')
     parser.add_argument('--password', '-p', help='database password')
     parser.add_argument('--ssl_key')
     parser.add_argument('--ssl_cert')
     parser.add_argument('--ssl_ca')
+    parser.add_argument('--dry_run', dest='dry_run', action='store_true')
+    parser.add_argument('--write_file', dest='dry_run', action='store_false')
+    parser.set_defaults(feature=True)
     args = parser.parse_args()
 
     main(args)
---
+++ api/isb_cgc_api/patients_annotations.py
@@ -0,0 +1,97 @@
+"""
+
+Copyright 2015, Institute for Systems Biology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+import endpoints
+import logging
+import MySQLdb
+
+from django.core.signals import request_finished
+from protorpc import remote, messages
+
+from isb_cgc_api_helpers import ISB_CGC_Endpoints, build_constructor_dict_for_message
+from message_classes import MetadataAnnotationItem
+from api.api_helpers import sql_connection
+
+logger = logging.getLogger(__name__)
+
+
+class MetadataAnnotationList(messages.Message):
+    items = messages.MessageField(MetadataAnnotationItem, 1, repeated=True)
+    count = messages.IntegerField(2, variant=messages.Variant.INT32)
+
+class PatientsAnnotationsQueryBuilder(object):
+
+    def build_query(self):
+        query_str = 'select * ' \
+                    'from metadata_annotation ' \
+                    'where ParticipantBarcode=%s'
+
+        return query_str
+
+
+@ISB_CGC_Endpoints.api_class(resource_name='patients')
+class PatientsAnnotationAPI(remote.Service):
+
+    GET_RESOURCE = endpoints.ResourceContainer(patient_barcode=messages.StringField(1, required=True))
+
+    @endpoints.method(GET_RESOURCE, MetadataAnnotationItem,
+                      path='patients/{patient_barcode}/annotations', http_method='GET')
+    def get(self, request):
+        """
+        Returns TCGA annotations about a specific patient,
+        Takes a patient barcode (of length 12, *eg* TCGA-B9-7268) as a required parameter.
+        User does not need to be authenticated.
+        """
+
+        cursor = None
+        db = None
+
+        patient_barcode = request.get_assigned_value('patient_barcode')
+        query_tuple = (str(patient_barcode),)
+        query_str = PatientsAnnotationsQueryBuilder().build_query()
+
+        try:
+            db = sql_connection()
+            cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+            # build annotation message
+            cursor.execute(query_str, query_tuple)
+            rows = cursor.fetchall()
+            if len(rows) == 0:
+                cursor.close()
+                db.close()
+                logger.warn("Patient barcode {} not found in metadata_annotation table.".format(patient_barcode))
+                raise endpoints.NotFoundException("Patient barcode {} not found".format(patient_barcode))
+
+            items = []
+            for row in rows:
+                constructor_dict = build_constructor_dict_for_message(MetadataAnnotationItem(), row)
+                items.append(MetadataAnnotationItem(**constructor_dict))
+
+            return MetadataAnnotationList(items=items, count=len(items))
+
+        except (IndexError, TypeError), e:
+            logger.info("Patient {} not found. Error: {}".format(patient_barcode, e))
+            raise endpoints.NotFoundException("Patient {} not found.".format(patient_barcode))
+        except MySQLdb.ProgrammingError as e:
+            logger.warn("Error retrieving patient, sample, or aliquot data: {}".format(e))
+            raise endpoints.BadRequestException("Error retrieving patient, sample, or aliquot data: {}".format(e))
+        finally:
+            if cursor: cursor.close()
+            if db and db.open: db.close()
+            request_finished.send(self)
