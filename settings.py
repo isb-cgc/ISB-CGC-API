920895dcda311eac1323940f6c12be5ad8b56015
S. Paquette <spaquett@systemsbiology.org>
2017-03-01 17:54:07 -0800

    -> Updating dev API to run under its own service

--- settings.py
+++ settings.py
@@ -61,8 +61,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.mysql'),
         'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
-        # 'PORT': os.environ.get('DATABASE_PORT', 3306),
-        'NAME': os.environ.get('DATABASE_NAME', 'dev'),
+        'NAME': os.environ.get('DATABASE_NAME', 'test'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD')
     }
@@ -110,12 +109,6 @@ def GET_BQ_COHORT_SETTINGS():
 
 USE_CLOUD_STORAGE = os.environ.get('USE_CLOUD_STORAGE')
 
-PROCESSING_ENABLED = os.environ.get('PROCESSING_ENABLED')
-PROCESSING_JENKINS_URL = os.environ.get('PROCESSING_JENKINS_URL')
-PROCESSING_JENKINS_PROJECT = os.environ.get('PROCESSING_JENKINS_PROJECT')
-PROCESSING_JENKINS_USER = os.environ.get('PROCESSING_JENKINS_USER')
-PROCESSING_JENKINS_PASSWORD = os.environ.get('PROCESSING_JENKINS_PASSWORD')
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -140,17 +133,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_FOLDER = os.environ.get('MEDIA_FOLDER')
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', '..', os.environ.get('MEDIA_FOLDER'))
-MEDIA_ROOT = os.path.normpath(MEDIA_ROOT)
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -159,7 +141,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
