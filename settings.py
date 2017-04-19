1cbda1b400ce3d774d10b3ab36b5142a6c2303c3
Michael Miller <mmiller@systemsbiology.org>
2017-04-19 15:33:16 -0700

    fix initialization error caused by using classes nested in a class; remove extra django import

--- settings.py
+++ settings.py
@@ -10,8 +10,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import django
-
 # Django settings for GAE_Django17 project.
 import os
 
