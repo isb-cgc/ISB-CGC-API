948ab6e4c147604dc8319674b9ad935035cca398
Michael Miller <mmiller@systemsbiology.org>
2017-07-21 10:30:28 -0700

    took into account new APIs for multiple whitelists

--- api_3/users_get_common.py
+++ api_3/users_get_common.py
@@ -15,47 +15,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
+import logging
 
 import endpoints
-import django
-import pytz
-import datetime
-import logging
 from protorpc import remote, messages
-from googleapiclient.errors import HttpError
 
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-from django.contrib.auth.models import User as Django_User
-from django.core.signals import request_finished
-from django.conf import settings
-
-from google_helpers.directory_service import get_directory_resource
-from accounts.models import NIH_User
+from dataset_utils.dataset_access_support_factory import DatasetAccessSupportFactory
 
 logger = logging.getLogger(__name__)
-
-CONTROLLED_ACL_GOOGLE_GROUP = settings.ACL_GOOGLE_GROUP
-INSTALLED_APP_CLIENT_ID = settings.INSTALLED_APP_CLIENT_ID
-
-
-def is_dbgap_authorized(user_email):
-    # TODOD: differentiate between TCGA and TARGET
-    directory_service, http_auth = get_directory_resource()
-    try:
-        directory_service.members().get(groupKey=CONTROLLED_ACL_GOOGLE_GROUP,
-                                        memberKey=user_email).execute(http=http_auth)
-        return True
-    except HttpError:
-        return False
-
 
 class UserGetAPIReturnJSON(messages.Message):
     message = messages.StringField(1)
     dbGaP_authorized = messages.BooleanField(2)
-
+    dbGaP_allowed = messages.BooleanField(3)
 
 class UserGetAPICommon(remote.Service):
-    def get(self, request):
+    def get(self, program):
         '''
         Returns the dbGaP authorization status of the user.
         '''
@@ -67,75 +42,33 @@ class UserGetAPICommon(remote.Service):
         if user_email is None:
             raise endpoints.UnauthorizedException("Authentication unsuccessful.")
 
-        # this checks the controlled-access google group
-        am_dbgap_authorized = is_dbgap_authorized(user_email)
-
-        if not am_dbgap_authorized:
+        das = DatasetAccessSupportFactory.from_webapp_django_settings()
+        authorized_datasets = das.get_datasets_for_era_login(user_email)
+        all_datasets = das.get_all_datasets_and_google_groups()
+        
+        authorized = False
+        allowed = False
+        for dataset in authorized_datasets:
+            if program in dataset:
+                authorized = True
+                allowed = True
+        if not allowed:
+            for dataset in all_datasets:
+                if program in dataset:
+                    allowed = True
+            
+        if not allowed:
             return UserGetAPIReturnJSON(message="{} is not on the controlled-access google group.".format(user_email),
                               dbGaP_authorized=False)
-
-        django.setup()
-
-        # all the following five situations should never happen
-
-        # 1. check to make sure they have an entry in auth_user
-        try:
-            django_user = Django_User.objects.get(email=user_email)
-        except (ObjectDoesNotExist, MultipleObjectsReturned), e:
-            logger.error("Email {} is in {} group but did not have a unique entry in auth_user table. Error: {}"
-                         .format(user_email, CONTROLLED_ACL_GOOGLE_GROUP, e))
-            request_finished.send(self)
-            raise endpoints.NotFoundException("{} is in the controlled-access google group "
-                                              "but does not have an entry in the user database."
-                                              .format(user_email))
-
-        # 2. check to make sure they have an entry in accounts_nih_user
-        try:
-            nih_user = NIH_User.objects.get(user_id=django_user.id)
-        except (ObjectDoesNotExist, MultipleObjectsReturned), e:
-            logger.error("Email {} is in {} group but did not have a unique entry in "
-                         "accounts_nih_user table. Error: {}"
-                         .format(user_email, CONTROLLED_ACL_GOOGLE_GROUP, e))
-            request_finished.send(self)
-            raise endpoints.NotFoundException("{} is in the controlled-access google group "
-                                              "but does not have an entry in the nih_user database."
-                                              .format(user_email))
-
-        # 3. check if their entry in accounts_nih_user is currently active
-        if not nih_user.active:
-            logger.error("Email {} is in {} group but their entry in accounts_nih_user table is inactive."
-                         .format(user_email, CONTROLLED_ACL_GOOGLE_GROUP))
-            request_finished.send(self)
-            raise endpoints.NotFoundException("{} is in the controlled-access google group "
-                                              "but has an inactive entry in the nih_user database."
-                                              .format(user_email))
-
-        # 4. check if their entry in accounts_nih_user is dbGaP_authorized
-        if not nih_user.dbGaP_authorized:
-            logger.error("Email {} is in {} group but their entry in accounts_nih_user table "
-                         "is not dbGaP_authorized."
-                         .format(user_email, CONTROLLED_ACL_GOOGLE_GROUP))
-            request_finished.send(self)
-            raise endpoints.NotFoundException("{} is in the controlled-access google group "
-                                              "but their entry in the nih_user database is not dbGaP_authorized."
-                                              .format(user_email))
-
-        # 5. check if their dbgap authorization has expired
-        expire_time = nih_user.NIH_assertion_expiration
-        expire_time = expire_time if expire_time.tzinfo is not None else pytz.utc.localize(expire_time)
-        now_in_utc = pytz.utc.localize(datetime.datetime.now())
-
-        if (expire_time - now_in_utc).total_seconds() <= 0:
-            logger.error("Email {} is in {} group but their entry in accounts_nih_user table "
-                         "is expired."
-                         .format(user_email, CONTROLLED_ACL_GOOGLE_GROUP))
-            request_finished.send(self)
-            raise endpoints.NotFoundException("{} is in the controlled-access google group "
-                                              "but their entry in the nih_user database is expired."
-                                              .format(user_email))
-
-        # all checks have passed
-        request_finished.send(self)
-        return UserGetAPIReturnJSON(message="{} has dbGaP authorization and is a member of the controlled-access google group."
-                          .format(user_email),
-                          dbGaP_authorized=True)
+        # since user has access to the program controlled data, include a warning
+        warn_message = 'You are reminded that when accessing controlled access information you are bound by the dbGaP DATA USE CERTIFICATION AGREEMENT (DUCA) for this dataset.'
+        if authorized:
+            return UserGetAPIReturnJSON(message="{} has dbGaP authorization for {} and is a member of the controlled-access google group.  {}"
+                              .format(user_email, program, warn_message),
+                              dbGaP_authorized=True,
+                              dbGaP_allowed=True)
+        else:
+            return UserGetAPIReturnJSON(message="{} has dbGaP authorization for {} but is not currently a member of the controlled-access google group.  {}"
+                              .format(user_email, program, warn_message),
+                              dbGaP_authorized=False,
+                              dbGaP_allowed=True)
