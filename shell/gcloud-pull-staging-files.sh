130b2440412182dc24bb136027c4d84777e7aa00
s-paquett <spaquett@systemsbiology.org>
2017-07-26 13:32:46 -0700

    -> Fix for UAT bucket name

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -3,7 +3,7 @@
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_JSON_FILE}" ./privatekey.json
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_USER_GCP_KEY}" ./
 
-./google-cloud-sdk/bin/gsutil cp "gs://${UAT_GCLOUD_BUCKET}/${UAT_DATASET_JSON_FILE}" ./
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_DATASET_JSON_FILE}" ./
 
 if [ -n "${UAT_NIH_AUTH_ON}" ]; then
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/saml/advanced_settings.json" ./saml/advanced_settings.json
