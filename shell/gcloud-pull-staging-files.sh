4f630773918b70d13b4b891e22f6f1700d5113aa
s-paquett <spaquett@systemsbiology.org>
2017-07-26 11:26:20 -0700

    -> Sprint 17 to UAT

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -3,10 +3,11 @@
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_JSON_FILE}" ./privatekey.json
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_USER_GCP_KEY}" ./
 
+./google-cloud-sdk/bin/gsutil cp "gs://${UAT_GCLOUD_BUCKET}/${UAT_DATASET_JSON_FILE}" ./
+
 if [ -n "${UAT_NIH_AUTH_ON}" ]; then
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/saml/advanced_settings.json" ./saml/advanced_settings.json
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/saml/settings.json" ./saml/settings.json
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/saml/certs/cert.pem" ./saml/certs/cert.pem
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/saml/certs/key.pem" ./saml/certs/key.pem
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/NIH_FTP.txt" ./NIH_FTP.txt
-./google-cloud-sdk/bin/gsutil cp "gs://${DEV_GCLOUD_BUCKET}/${DEV_DATASET_JSON_FILE}" ./
