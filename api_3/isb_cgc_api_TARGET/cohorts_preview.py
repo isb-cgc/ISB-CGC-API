28232968e3188e70902000917310f4f7f5b45963
Michael Miller <mmiller@systemsbiology.org>
2017-10-06 14:25:33 -0700

    modify preview endpoints to group fields by table

--- api_3/isb_cgc_api_TARGET/cohorts_preview.py
+++ api_3/isb_cgc_api_TARGET/cohorts_preview.py
@@ -23,7 +23,7 @@ from message_classes import MetadataRangesItem
 @ISB_CGC_TARGET_Endpoints.api_class(resource_name='cohorts')
 class TARGET_CohortsPreviewAPI(CohortsPreviewHelper):
 
-    GET_RESOURCE = endpoints.ResourceContainer(**{field.name: field for field in MetadataRangesItem.all_fields()})
+    GET_RESOURCE = endpoints.ResourceContainer(MetadataRangesItem)
 
     @endpoints.method(GET_RESOURCE, CohortsPreviewHelper.CohortCasesSamplesList, path='target/cohorts/preview', http_method='GET')
     def preview(self, request):
