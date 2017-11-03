5a536d6d8636e9eb863abe1085e814766d2dc1c9
Michael Miller <mmiller@systemsbiology.org>
2017-11-03 15:44:02 -0700

    construct the return type properly

--- api_3/isb_cgc_api/files_get_file_paths.py
+++ api_3/isb_cgc_api/files_get_file_paths.py
@@ -60,6 +60,7 @@ class FilesGetPath(remote.Service):
                         paths += row[1]
                 except Exception as e:
                     print 'problem executing sql({}):\n\t{}\n\t{}'.format(e, sql, params)
-        
-        return [item for sublist in uuid2paths.values() for item in sublist]
+        filepaths = FilesGetPath.FilePaths()
+        filepaths.paths = [item for sublist in uuid2paths.values() for item in sublist]
+        return paths
 
