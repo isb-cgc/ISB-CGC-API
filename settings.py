21650504ba9d4cad26f9ad4c665d898209c41423
s-paquett <spaquett@systemsbiology.org>
2018-03-22 18:43:10 -0700

    -> Fix IS_DEV

--- settings.py
+++ settings.py
@@ -26,13 +26,10 @@ ALLOWED_HOSTS = [
 ### Check what we're running in
 APP_ENGINE_FLEX = 'aef-'
 APP_ENGINE = 'Google App Engine/'
+IS_DEV = (os.environ.get('IS_DEV', 'False') == 'True')
 IS_APP_ENGINE_FLEX = os.getenv('GAE_INSTANCE', '').startswith(APP_ENGINE_FLEX)
 IS_APP_ENGINE = os.getenv('SERVER_SOFTWARE', '').startswith(APP_ENGINE)
 IS_DEV = bool(os.environ.get('IS_DEV', False))
-
-#ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-#)
 
 ADMINS = ()
 MANAGERS = ADMINS
