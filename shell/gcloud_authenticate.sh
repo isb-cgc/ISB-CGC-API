10c0590e767f30196147db9b3f0d9d56813df573
s-paquett <spaquett@systemsbiology.org>
2018-06-20 15:47:39 -0700

    -> CircleCI 2.0

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,11 +1,5 @@
-touch dev_deployment.key.json
-echo "{\
-  \"private_key_id\": \"$DEV_PRIVATE_KEY_ID\",\
-  \"private_key\": \"$DEV_PRIVATE_KEY\",\
-  \"client_email\": \"$DEV_CLIENT_EMAIL\",\
-  \"client_id\": \"$DEV_CLIENT_ID\",\
-  \"type\": \"service_account\"\
-}" | tee dev_deployment.key.json > /dev/null 2> /dev/null
+echo $DEV_PRIVATE_KEY_FOR_V2 | base64 --decode --ignore-garbage > deployment.key.json
+
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file dev_deployment.key.json
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $DEV_CLIENT_EMAIL
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID"
