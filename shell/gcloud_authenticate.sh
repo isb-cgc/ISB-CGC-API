45d1549f41b37065f12819f31e2c64dd0bb65541
s-paquett <spaquett@systemsbiology.org>
2018-06-20 15:56:46 -0700

    -> No more /ubuntu, all is /circleci

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,5 +1,5 @@
 echo $DEV_PRIVATE_KEY_FOR_V2 | base64 --decode --ignore-garbage > deployment.key.json
 
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file dev_deployment.key.json
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $DEV_CLIENT_EMAIL
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID"
+/home/circleci/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file dev_deployment.key.json
+/home/circleci/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $DEV_CLIENT_EMAIL
+/home/circleci/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID"
