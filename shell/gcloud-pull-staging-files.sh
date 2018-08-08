208e712baf7373bb367840e53df0e76e7469e937
s-paquett <spaquett@systemsbiology.org>
2018-08-07 19:07:41 -0700

    -> Merge shenanigans fixed

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -6,10 +6,10 @@ mkdir ./txt
 ./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/${TEST_JSON_FILE}" ./privatekey.json
 ./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/${TEST_USER_GCP_KEY}" ./
 
-./google-cloud-sdk/bin/gsutil cp "gs://${DEV_GCLOUD_BUCKET}/${SERVICE_ACCOUNT_BLACKLIST_JSON_FILE}" ./
-./google-cloud-sdk/bin/gsutil cp "gs://${DEV_GCLOUD_BUCKET}/${GOOGLE_ORG_WHITELIST_JSON_FILE}" ./
-./google-cloud-sdk/bin/gsutil cp "gs://${DEV_GCLOUD_BUCKET}/${MANAGED_SERVICE_ACCOUNTS_JSON_FILE}" ./
-./google-cloud-sdk/bin/gsutil cp "gs://${DEV_GCLOUD_BUCKET}/${TEST_GCLOUD_BUCKET}" ./
+./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/${SERVICE_ACCOUNT_BLACKLIST_JSON_FILE}" ./
+./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/${GOOGLE_ORG_WHITELIST_JSON_FILE}" ./
+./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/${MANAGED_SERVICE_ACCOUNTS_JSON_FILE}" ./
+./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/${TEST_DATASET_JSON_FILE}" ./
 
 if [ -n "${TEST_NIH_AUTH_ON}" ]; then
   ./google-cloud-sdk/bin/gsutil cp "gs://${TEST_GCLOUD_BUCKET}/saml/advanced_settings.json" ./saml/advanced_settings.json
