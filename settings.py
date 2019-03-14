f2344163c38f2afaf7094864424ada58f68245b6
s-paquett <spaquett@systemsbiology.org>
2019-03-13 17:55:05 -0700

    -> ...need py3 print

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ for directory_name in SHARED_SOURCE_DIRECTORIES:
 
 DEBUG                   = (os.environ.get('DEBUG', 'False') == 'True')
 
-print >> sys.stdout, "[STATUS] DEBUG mode is "+str(DEBUG)
+print("[STATUS] DEBUG mode is "+str(DEBUG))
 
 ALLOWED_HOSTS = list(set(os.environ.get('ALLOWED_HOST', 'localhost').split(',') + ['localhost', '127.0.0.1', '[::1]', gethostname(), gethostbyname(gethostname()),]))
 # Testing health checks problem
