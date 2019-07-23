f37b9810cda2c970b7322eaed035d52b73822724
s-paquett <spaquett@systemsbiology.org>
2019-07-22 18:35:31 -0700

    -> More fixes

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,7 +1,7 @@
-echo ${KEY} | base64 --decode --ignore-garbage > deployment.key.json
+echo ${DEPLOYMENT_KEY} | base64 --decode --ignore-garbage > deployment.key.json
 
 gcloud auth activate-service-account --key-file deployment.key.json
-echo "Setting deployment client email to ${CLIENT_EMAIL}"
-gcloud config set account $CLIENT_EMAIL
-echo "Setting deployment project to ${PROJECT_ID}"
-gcloud config set project "$PROJECT_ID"
+echo "Setting deployment client email to ${DEPLOYMENT_CLIENT_EMAIL}"
+gcloud config set account $DEPLOYMENT_CLIENT_EMAIL
+echo "Setting deployment project to ${DEPLOYMENT_PROJECT_ID}"
+gcloud config set project "$DEPLOYMENT_PROJECT_ID"
