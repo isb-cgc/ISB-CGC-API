842bab0c5b0bbf1d473a6062338ff2ea6136a83d
s-paquett <spaquett@systemsbiology.org>
2019-08-05 23:28:55 -0700

    -> or...

--- shell/pull_config.sh
+++
@@ -1,5 +0,0 @@
-gsutil cp gs://${DEPLOYMENT_BUCKET}/deployment_config.txt ./
-if [ ! -f "deployment_config.txt" ]; then
-  echo "[ERROR] Couldn't assign deployment configuration file - exiting."
-  exit 1
-fi
