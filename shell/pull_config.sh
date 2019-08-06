b79e7dcc6435e0e9ab42982ccc4212063eb34374
s-paquett <spaquett@systemsbiology.org>
2019-08-05 23:14:23 -0700

    -> Try separate script

---
+++ shell/pull_config.sh
@@ -0,0 +1,7 @@
+gsutil cp gs://${DEPLOYMENT_BUCKET}/delpoyment_config.txt ./
+if [ ! -f "delpoyment_config.txt" ]; then
+  echo "[ERROR] Couldn't assign deployment configuration file - exiting."
+  exit 1
+else
+  xargs -a config_file.txt -I{} echo "export {}" >> $BASH_ENV
+fi
