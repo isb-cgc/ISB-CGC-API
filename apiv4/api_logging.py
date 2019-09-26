21d76b1e849ad1d50e527afacba923295ee42001
s-paquett <spaquett@systemsbiology.org>
2019-09-25 17:18:05 -0700

    -> Log to stackdriver

--- apiv4/api_logging.py
+++ apiv4/api_logging.py
@@ -3,6 +3,6 @@ from google_helpers.stackdriver import StackDriverLogger
 
 # Log all API activity to StackDriver
 st_logger = StackDriverLogger.build_from_django_settings()
-log_name = settings.API_ACTIVITY_LOG
+log_name = settings.API_ACTIVITY_LOG_NAME
 user_activity_message = "[USER API CALL] User {} performing method {} path {}"
 activity_message = "[API CALL] Saw method {} for path {}"
