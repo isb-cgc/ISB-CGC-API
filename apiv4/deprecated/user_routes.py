1a7f1f474d1ac1348c425ff08ccd96d5a8425f50
s-paquett <spaquett@systemsbiology.org>
2019-12-12 14:49:11 -0800

    -> Simplify deprecation module

--- apiv4/deprecated/user_routes.py
+++ apiv4/deprecated/user_routes.py
@@ -17,14 +17,15 @@
 import logging
 import json
 from apiv4 import app
+from flask import jsonify, request
 
 HTTP_405_METHOD_NOT_ALLOWED = 405
 
 
-def external_url_for(for_this):
+def make_405_response():
     response = jsonify({
         'code': HTTP_405_METHOD_NOT_ALLOWED,
-        'message': "The 'programs' path has been deprecated in version 4.1 in favor of /data/availabile and subroutes."
+        'message': "The 'gcp' path has been deprecated in version 4.1 in favor of /cloud_projects and subroutes."
     })
 
     response.status_code = HTTP_405_METHOD_NOT_ALLOWED
@@ -34,35 +35,14 @@ def external_url_for(for_this):
 
 @app.route('/v4/users/gcp/validate/<gcp_id>/', methods=['GET'], strict_slashes=False)
 def validate_gcp_old(gcp_id):
-    response = jsonify({
-        'code': HTTP_405_METHOD_NOT_ALLOWED,
-        'message': "The 'gcp' path has been deprecated in version 4.1 in favor of /cloud_projects and subroutes."
-    })
-
-    response.status_code = HTTP_405_METHOD_NOT_ALLOWED
-
-    return response
+    return make_405_response()
 
 
 @app.route('/v4/users/gcp/<gcp_id>/', methods=['DELETE', 'PATCH', 'GET'], strict_slashes=False)
 def user_gcp_old(gcp_id):
-    response = jsonify({
-        'code': HTTP_405_METHOD_NOT_ALLOWED,
-        'message': "The 'gcp' path has been deprecated in version 4.1 in favor of /cloud_projects and subroutes."
-    })
-
-    response.status_code = HTTP_405_METHOD_NOT_ALLOWED
-
-    return response
+    return make_405_response()
 
 
 @app.route('/v4/users/gcp/', methods=['POST', 'GET'], strict_slashes=False)
 def user_gcps_old():
-    response = jsonify({
-        'code': HTTP_405_METHOD_NOT_ALLOWED,
-        'message': "The 'gcp' path has been deprecated in version 4.1 in favor of /cloud_projects and subroutes."
-    })
-
-    response.status_code = HTTP_405_METHOD_NOT_ALLOWED
-
-    return response
+    return make_405_response()
