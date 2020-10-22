21e26312e59d73de08984ebbcae92121539aa11f
BillClifford <bcli4d>
2020-10-22 08:31:32 -0700

    Eliminated test of /help. No longer needed.

--- tests/test_main_routes.py
+++ tests/test_main_routes.py
@@ -20,10 +20,6 @@ def test_about(client, app):
     assert client.get('/v1/about').status_code == 200
     assert 'NCI IDC API' in response.json['message']
 
-def test_help(client,app):
-    response = client.get('v1/help/')
-    assert response.status_code == 200
-    assert "System Documentation" in response.json['data']
 
 def test_oauth2callback(client, app):
     response = client.get('v1/oauth2callback')
