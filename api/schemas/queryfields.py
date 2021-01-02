516e6fcbea8ad761aa7e207b8ade12036700439e
BillClifford <bcli4d>
2021-01-02 15:13:36 -0800

    Fixed body param

---
+++ api/schemas/queryfields.py
@@ -0,0 +1,57 @@
+QUERY_FIELDS={
+  "type": "object",
+  "properties": {
+    "fields": {
+      "type": "array",
+      "items": {
+        "type": "string",
+        "enum": [
+          "Modality",
+          "BodyPartExamined",
+          "StudyDescription",
+          "StudyInstanceUID",
+          "PatientID",
+          "SeriesInstanceUID",
+          "SOPInstanceUID",
+          "SeriesDescription",
+          "SliceThickness",
+          "SeriesNumber",
+          "StudyDate",
+          "SOPClassUID",
+          "collection_id",
+          "AnatomicRegionSequence",
+          "SegmentedPropertyCategoryCodeSequence",
+          "SegmentedPropertyTypeCodeSequence",
+          "FrameOfReferenceUID",
+          "SegmentNumber",
+          "SegmentAlgorithmType",
+          "SUVbw",
+          "Volume",
+          "Diameter",
+          "Surface_area_of",
+          "Total_Lesion",
+          "Standardized_Added_Metabolic",
+          "Percent_Within_First_Quarter_of_Intensity",
+          "Percent_Within_Third_Quarter_of_Intensity",
+          "Percent_Within_Fourth_Quarter_of_Intensity",
+          "Percent_Within_Second_Quarter_of_Intensity",
+          "Standardized_Added_Metabolic_Activity",
+          "Glycolysis_Within_First_Quarter_of_Intensity",
+          "Glycolysis_Within_Third_Quarter_of_Intensity",
+          "Glycolysis_Within_Fourth_Quarter_of_Intensity",
+          "Glycolysis_Within_Second_Quarter_of_Intensity",
+          "Internal",
+          "Sphericity",
+          "Calcification",
+          "Lobular",
+          "Spiculation",
+          "Margin",
+          "Texture",
+          "Subtlety",
+          "Malignancy"
+        ]
+      }
+    }
+  },
+  "$schema": "http://json-schema.org/schema#"
+}
