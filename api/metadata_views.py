ea4b927bcc78c627897eea3ba49d7d6a9df87eeb
BillClifford <bcli4d>
2021-05-26 13:07:29 -0700

    Added some debugging logs

--- api/metadata_views.py
+++ api/metadata_views.py
@@ -57,8 +57,7 @@ def get_versions():
         logger.error("[ERROR] No content in response from web app")
         logger.error("[ERROR] status_code: {}".format(response.status_code))
         logger.exception(e)
-        logger.error("[ERROR] Response type: %s", type(response))
-        logger.error("[ERROR] Response: %s", response)
+        logger.error("[ERROR] Response dictionary: %s", response.__dict__)
 
         return dict(
             message="Encountered an error while retrieving the versions list.",
