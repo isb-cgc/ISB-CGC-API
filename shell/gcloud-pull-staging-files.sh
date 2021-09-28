be12ae912a66380195cbd825195bc559ae4f6aa9
S. Paquette <spaquett@systemsbiology.org>
2021-09-28 10:57:34 -0700

    - Removing old stuff

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -6,8 +6,6 @@ gsutil cp "gs://${DEPLOYMENT_BUCKET}/${API_RUNTIME_SA_KEY}" ./privatekey.json
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${OPEN_API_YAML}" ./openapi-appengine.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${OPEN_API_YAML}" ./api/api.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${API_APP_YAML}" ./app.yaml
-gsutil cp "gs://${DEPLOYMENT_BUCKET}/${USER_GCP_KEY}" ./
-gsutil cp "gs://${DEPLOYMENT_BUCKET}/${DCF_SECRETS_FILE}" ./dcf_secrets.txt
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${API_TOKEN_FILE}" ./api_token.txt
 
 # Pack staged files for caching
