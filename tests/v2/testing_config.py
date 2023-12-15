3ec385b598e6adc8462a8e268d86eb1a9deca670
BillClifford <bcli4d>
2023-12-15 11:04:28 -0800

    Get new MySQL version

--- tests/v2/testing_config.py
+++ tests/v2/testing_config.py
@@ -26,7 +26,7 @@ API_VERSION = 'v2'
 VERSIONS = 17
 NUM_COLLECTIONS = 142
 
-test_dev_api = False
+test_dev_api = True
 dev_api_requester = requests
 API_URL = f'https://dev-api.canceridc.dev/{API_VERSION}' if test_dev_api else f'{API_VERSION}'
 get_data = dev_response.json if test_dev_api else local_resonse.get_json
