f5a4c582795a91a926aae1a6803ff9c18253318e
BillClifford <bcli4d>
2023-12-18 13:13:58 -0800

    Resubmitting.

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -3,7 +3,6 @@ mkdir ./txt
 
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${ENV_FILE}" ./.env
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${API_RUNTIME_SA_KEY}" ./privatekey.json
-#gsutil cp "gs://${DEPLOYMENT_BUCKET}/${OPEN_API_YAML}" ./openapi-appengine.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${OPEN_API_V1_YAML}" ./openapi-appengine.v1.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${OPEN_API_V2_YAML}" ./openapi-appengine.v2.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${OPEN_API_YAML}" ./api/api.yaml
