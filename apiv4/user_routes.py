6a969401b8324d99b8eb148d9856f1ade3c13bf3
s-paquett <spaquett@systemsbiology.org>
2024-03-27 18:44:53 -0700

    -> Remove GCP reg

--- apiv4/user_routes.py
+++ apiv4/user_routes.py
@@ -19,7 +19,7 @@ import json
 from flask import jsonify, request, redirect, url_for
 from apiv4 import app
 from auth import auth_info, UserValidationException, validate_user, get_user
-from user_views import get_user_acls, get_account_details, gcp_validation, gcp_registration, gcp_unregistration, gcp_info
+from user_views import get_user_acls, get_account_details
 from django.conf import settings
 from django.db import close_old_connections
 from api_logging import *
