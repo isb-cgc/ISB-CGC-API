fb260dfc6e10dd2144c20d6cf761d1114a8baba0
BillClifford <bcli4d>
2024-05-01 11:24:05 -0700

    Fix idc_auth.py to work when called from an app. testing_config.py must call idc_auth according to path being testing.

--- scripts/idc_auth.py
+++ scripts/idc_auth.py
@@ -17,6 +17,15 @@
 #
 # May be run from the command line or in scripts/ipython.
 #
+# The oauth2client functionality is called from your application and runs
+# through all the steps to obtain credentials. It takes a ``Flow``
+# argument and attempts to open an authorization server page in the
+# user's default web browser. The server asks the user to grant your
+# application access to the user's data. If the user grants access,
+# the ``run()`` function returns new credentials. The new credentials
+# are also stored in the ``storage`` argument, which updates the file
+# associated with the ``Storage`` object.
+#
 # The resulting credentials file can be copied to any machine from which you want
 # to access the API.
 #
@@ -43,26 +52,19 @@
 #     credentials = get_credentials(storage=storage)
 # # Notes:
 #
-## The script requires version 4.1.3 of the oauth2client module.
+# The script requires version 4.1.3 of the oauth2client module.
 #
-## In some cases you might not want your credentials written to the local files system. In such a case,
-## use /dev/null as the storage_file:
-##     from oauth2client.file import Storage
-##     import idc_auth
-##     import os
-##
-##     storage_file = "/dev/null"
-##     storage = Storage(storage_file)
-##     credentials = get_credentials(storage=storage)
+# In some cases you might not want your credentials written to the local files system. In such a case,
+# use /dev/null as the storage_file:
+#     from oauth2client.file import Storage
+#     import idc_auth
+#     import os
 #
-## The OAuth2WebServerFlow() outputs a message like:
-##    Your browser has been opened to visit:
-##        https://accounts.google.com/o/oauth2/v2/auth?client_id=...
-##    If your browser is on a different machine then exit and re-run this
-##    application with the command-line parameter
-##      --noauth_local_webserver
-## The oauth2client module no longer supports the noauth_local_webserver option. Therefore you should
-## run this script on a machine that is running a browser.
+#     storage_file = "/dev/null"
+#     storage = Storage(storage_file)
+#     credentials = get_credentials(storage=storage)
+#
+# The oauth2client module must be run on a machine that is running a browser.
 
 from argparse import ArgumentParser
 import os
@@ -73,6 +75,7 @@ pkg_resources.require("oauth2client==4.1.3")
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client import tools
 from oauth2client.file import Storage
+from scripts.idc_auth import get_credentials
 
 VERBOSE = False
 CLIENT_ID = '198650116749-jold6g3160renk0nm566m7tguvpdktga.apps.googleusercontent.com'
@@ -85,26 +88,31 @@ CLIENT_SECRET = 'tQdFIEHMlueJAa0vnKNPfArS'
 EMAIL_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
 CLOUD_PLATFORM_SCOPE='https://www.googleapis.com/auth/cloud-platform'
 DEFAULT_STORAGE_FILE = os.path.join(os.path.expanduser("~"), '.idc_credentials')
-
+class flags():
+    auth_host_name='localhost'
+    noauth_local_webserver=False
+    auth_host_port=[8080, 8090]
+    logging_level='ERROR'
+FLAGS = flags()
 
 def maybe_print(msg):
     if VERBOSE:
         print(msg)
 
 
-def get_credentials(storage=None):
+def get_credentials(storage=None, flags=FLAGS):
     if storage is None:
         storage = Storage(DEFAULT_STORAGE_FILE)
     credentials = storage.get()
     # if not credentials or credentials.invalid or credentials.access_token_expired:
-    if credentials.access_token_expired:
+    if not credentials or credentials.invalid:
+        maybe_print('credentials missing/invalid/expired, kicking off OAuth flow')
+        flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, EMAIL_SCOPE, pkce=True)
+        credentials = tools.run_flow(flow, storage, flags)
+    elif credentials.access_token_expired:
         if credentials.access_token_expired:
             h = httplib2.Http()
             credentials.refresh(h)
-    elif not credentials or credentials.invalid:
-        maybe_print('credentials missing/invalid/expired, kicking off OAuth flow')
-        flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, EMAIL_SCOPE, pkce = True)
-        credentials = tools.run_flow(flow, storage)
     return credentials
 
 
@@ -126,7 +134,8 @@ def main():
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('--storage_file', '-s', default=DEFAULT_STORAGE_FILE, help='storage file to use for the credentials (default is {})'.format(DEFAULT_STORAGE_FILE))
+    # parser.add_argument('--storage_file', '-s', default=DEFAULT_STORAGE_FILE, help='storage file to use for the credentials (default is {})'.format(DEFAULT_STORAGE_FILE))
+    parser.add_argument('--storage_file', '-s', default='/Users/BillClifford/.idc_credentials_test', help='storage file to use for the credentials (default is {})'.format(DEFAULT_STORAGE_FILE))
     parser.add_argument('--verbose', '-v', dest='verbose', action='store_false', help='display credentials storage location, access token, and refresh token')
     parser.set_defaults(verbose=False)
     parser.add_argument('--client_id', default=CLIENT_ID)
