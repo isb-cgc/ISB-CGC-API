d8416ed0bc45733b79b9752a0897536d0cee1b6b
s-paquett <spaquett@systemsbiology.org>
2024-06-27 18:08:35 -0700

    -> Logging adjustments

--- apiv4/__init__.py
+++ apiv4/__init__.py
@@ -25,6 +25,13 @@ from flask_cors import cross_origin
 from flask_talisman import Talisman
 import google.cloud.logging
 
+# We need to hook up Python logging to Google Cloud Logging for AppEngine (or nothing will be logged)
+client = google.cloud.logging.Client()
+client.get_default_handler()
+client.setup_logging()
+
+logger = logging.getLogger(settings.LOGGER_NAME)
+
 app = Flask(__name__, static_folder='api_static')
 
 Talisman(app, strict_transport_security_max_age=300, content_security_policy={
@@ -49,14 +56,6 @@ from sample_case_routes import *
 from file_routes import *
 from user_routes import *
 from deprecated.user_routes import *
-
-# We need to hook up Python logging to Google Cloud Logging for AppEngine (or nothing will be logged)
-client = google.cloud.logging.Client()
-client.get_default_handler()
-client.setup_logging()
-
-logger = logging.getLogger(settings.LOGGER_NAME)
-
 
 @app.context_processor
 def utilities():
