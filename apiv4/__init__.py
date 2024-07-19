88c1405ac396d288dfbbbec58033e7245ef58963
s-paquett <spaquett@systemsbiology.org>
2024-07-18 19:47:40 -0700

    -> Fun with logs

--- apiv4/__init__.py
+++ apiv4/__init__.py
@@ -38,7 +38,7 @@ dictConfig({
     }},
     'handlers': {'wsgi': {
         'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'stream': 'ext://sys.stdout',
         'formatter': 'default'
     }},
     'root': {
